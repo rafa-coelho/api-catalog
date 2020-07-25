8dffe3411b543bc57dc2aa6f836868d9147f8a61
Wellington Nascimento <wellingtonwsn2015@gmail.com>
2020-07-25 13:33:54 -0300

    metodo logou

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -3,6 +3,7 @@ const axios = require('axios').default;
 
 //Pegando o express
 module.exports = (app) => {
+    //Endpoint login
     app.post('/login', async (req,res) => {
 
         //Error
@@ -13,7 +14,6 @@ module.exports = (app) => {
         }
         
         const {body} = req;
-        console.log(body)
         let xml = '';
         xml += `<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ser="http://www.ca.com/UnicenterServicePlus/ServiceDesk">`;
         xml += `   <soapenv:Header/>`;
@@ -42,7 +42,38 @@ module.exports = (app) => {
             res.send(response)
         };
       
-        
-
     });  
+
+    //Endpoint logout
+    app.post('/logout', async(req,res)=>{
+
+        const response = {
+            data: "",
+            msg:"",
+            status: 1
+        };
+
+        const {headers} = req;
+        const sid = headers['authorization'];
+
+        let xml = '';
+        xml += '<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ser="http://www.ca.com/UnicenterServicePlus/ServiceDesk">'
+        xml += '    <soapenv:Header/>'
+        xml += '    <soapenv:Body>'
+        xml += '        <ser:logout>'
+        xml += '            <sid>'+sid+'</sid>'
+        xml += '        </ser:logout>'
+        xml += '    </soapenv:Body>'
+        xml += '</soapenv:Envelope>'
+
+        try{
+            await axios.post(HOST_SDM,xml,{headers:{'SOAPAction':'http://www.ca.com/UnicenterServicePlus/ServiceDesk/logout'}});
+        }catch(e){
+            console.log(e);
+        }finally{
+            res.send(response);
+        };
+        
+    });
+    
 };
