9332abe898121fca693db778d9048481fcce0086
Wellington Nascimento <wellingtonwsn2015@gmail.com>
2020-07-25 12:32:24 -0300

    Metodo de login

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,5 +1,48 @@
+//Pegando Axios - Faz requisições em HTTP ou HTTPS
+const axios = require('axios').default;
+
+//Pegando o express
 module.exports = (app) => {
-    app.get('/', (req,res) => {
-        res.send("Sua irmãa")
-    });
-}
+    app.post('/login', async (req,res) => {
+
+        //Error
+        const response = {
+            data: "",
+            msg:"",
+            status: 0
+        }
+        
+        const {body} = req;
+        console.log(body)
+        let xml = '';
+        xml += `<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ser="http://www.ca.com/UnicenterServicePlus/ServiceDesk">`;
+        xml += `   <soapenv:Header/>`;
+        xml += `   <soapenv:Body>`;
+        xml += `      <ser:login>`;
+        xml += `         <username>${body.username}</username>`;
+        xml += `         <password>${body.password}</password>`;
+        xml += `      </ser:login>`;
+        xml += `   </soapenv:Body>`;
+        xml += `</soapenv:Envelope>`;
+
+        try{
+            const post = await axios.post(HOST_SDM,xml,{headers:{'SOAPAction': 'http://www.ca.com/UnicenterServicePlus/ServiceDesk/login'}});
+
+            const sid = (/<loginReturn xmlns="">(.*?)<\/loginReturn>/g).exec(post.data)[1];
+            
+            if(Number(sid)){
+               response.data = sid;
+               response.msg = "Login feito com sucesso";
+               response.status = 1;
+            };
+        }catch(e){
+            response.msg = "Error ao fazer login";
+        }finally{
+            
+            res.send(response)
+        };
+      
+        
+
+    });  
+};
--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json())
+
+global.HOST_SDM = "https://sdm17.sondait.com.br:8080/axis/services/USD_R11_WebService";
 const consign = require('consign');
 consign().include('controllers').into(app);
 
 
-
-app.listen(3333, () => {});
-
-
-
+app.listen(3333, () => {
+    for (let index = 0; index < 10; index++) console.log()
+    console.clear();
+    console.log("---------------------------")
+    console.log("Rodando na porta 3333");
+});
