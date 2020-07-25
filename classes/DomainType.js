320ca84eca83050e0b204ddd97e1a118f949f641
Rafael Coelho <rafaelcoelho8@yahoo.com.br>
2020-07-25 17:56:26 -0300

    added: Domain-Value classes

---
+++ classes/DomainType.js
@@ -0,0 +1,8 @@
+class DomainType extends Classes{
+
+    static table = 'domain_type';
+    static fields = [ 'id', 'name', 'description' ];
+
+}
+
+module.exports = DomainType;
---
+++ classes/DomainValue.js
@@ -0,0 +1,8 @@
+class DomainValue extends Classes{
+
+    static table = 'domain_value';
+    static fields = [ 'id', 'value', 'type', 'parent' ];
+
+}
+
+module.exports = DomainValue;
