8e7ce2adc0a5824cdc3dd195cfafbe9140875837
Rafael Coelho <rafaelcoelho8@yahoo.com.br>
2020-07-27 21:33:23 -0300

    added: OfferingFieldOption class

---
+++ classes/OfferingFieldOption.js
@@ -0,0 +1,7 @@
+class OfferingFieldOption extends Classes
+{
+    static table = 'offering_field_option';
+    static fields = [ 'id', 'field', 'label', 'value' ];
+}
+
+module.exports = OfferingFieldOption;
