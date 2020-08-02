b728caf5b51a289939e1fb4e68fdff525256a6fb
Rafael Coelho <rafaelcoelho8@yahoo.com.br>
2020-08-02 01:30:52 -0300

    updated: DB.Get()

--- utils/DB.js
+++ utils/DB.js
@@ -1,4 +1,7 @@
+const knex = require('../database/connection');
 const db = require("mysql");
+const { off, whereRaw } = require('../database/connection');
+
 
 const con = db.createPool({
     host:  "127.0.0.1",
@@ -23,17 +26,27 @@ class DB
     }
     
     
-    Get(callback){
-        return new Promise(resolve => {
-            const where = (this.where != "" && this.where != undefined) ? " WHERE " + this.where : "";
-            const order_by = (this.order_by) ? " ORDER BY " + this.order_by : "";
-            const limit = (this._limit) ? " LIMIT " + this._limit : "";
-            con.query("SELECT * FROM " + this.table + where + order_by + limit, (err, result) => {
-                if(typeof callback == "function")
-                    callback(result);
-                resolve(result);
-            });
-        });
+    async Get(callback){
+        const where = (this.where != "" && this.where != undefined) ? this.where : "";
+        const order_by = (this.order_by) ? this.order_by : "id desc";
+        let limit = (this._limit) ? this._limit : null;
+        let offset = (this._offset) ? this._offset : 0;
+
+        
+        if(limit){
+            if(limit.toString().indexOf(',') >= 0){
+                offset = limit.split(',')[0].replace(/\D+/g, '')
+                limit = limit.split(',')[1].replace(/\D+/g, '')
+            }
+        }
+
+        const data = await knex(this.table)
+            .whereRaw(where)
+            .orderByRaw(order_by)
+            .limit(limit)
+            .offset(offset);
+
+        return data;
     }
     
     Insert(callback){
