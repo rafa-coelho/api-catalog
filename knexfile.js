2aecad37c534a8e0ce6fa95746306682a0ba1b26
Rafael Coelho <rafaelcoelho8@yahoo.com.br>
2020-08-03 17:38:53 -0300

    added: database migrate script

--- knexfile.js
+++ knexfile.js
@@ -1,19 +1,5 @@
 const path = require('path');
 
-if(process.env.NODE_ENV === 'prod'){
-    const conn = {
-        host: process.env.DB_HOST ? process.env.DB_HOST : "127.0.0.1",
-        user: process.env.DB_USER ? process.env.DB_USER : "root",
-        password: process.env.DB_PASS ? process.env.DB_PASS : "",
-        charset: 'utf8'
-    };
-
-    try{
-        const knex = require('knex')({ client: 'mysql', connection: conn });
-        knex.raw(`CREATE DATABASE ${process.env.DB_BASE ? process.env.DB_BASE : "catalog"}`).then(() => { }).catch(e => {});
-    }catch(e){}
-}
-
 module.exports = {
     development: {
         client: 'sqlite3',
