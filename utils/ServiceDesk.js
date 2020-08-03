27fcc22915b3768a692ddb354fd327000b508765
Rafael Coelho <rafaelcoelho8@yahoo.com.br>
2020-08-03 12:38:16 -0300

    added: ServiceDesk.VerificaSessao()

--- utils/ServiceDesk.js
+++ utils/ServiceDesk.js
@@ -1,4 +1,7 @@
 const axios = require('axios');
+const Util = require('./Util');
+
+// TODO: ErrorCode 1010 treatment (bad session)
 
 class ServiceDesk
 {
@@ -38,6 +41,53 @@ class ServiceDesk
         }
 
         return resp
+    }
+
+    static async VerificarSessao(session_id){
+        const resp = {
+            status: 0
+        };
+
+        let xml = '';
+        xml += `<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ser="http://www.ca.com/UnicenterServicePlus/ServiceDesk">`;
+        xml += `   <soapenv:Header/>`;
+        xml += `   <soapenv:Body>`;
+        xml += `      <ser:doSelect>`;
+        xml += `         <sid>${session_id}</sid>`;
+        xml += `         <objectType>session_log</objectType>`;
+        xml += `         <whereClause>session_id = ${session_id} AND logout_time is null</whereClause>`;
+        xml += `         <maxRows>1</maxRows>`;
+        xml += `         <attributes>`;
+        xml += `         </attributes>`;
+        xml += `      </ser:doSelect>`;
+        xml += `   </soapenv:Body>`;
+        xml += `</soapenv:Envelope>`;
+
+        try{
+            const post = await axios.post(HOST_SDM, xml, {
+                headers:{ 'SOAPAction': 'http://www.ca.com/UnicenterServicePlus/ServiceDesk/USD_WebServiceSoap/doSelectRequest' }
+            });
+
+            const json = await Util.xmlToJson(post.data);
+
+            if(!json.UDSObjectList){
+                // Erro no SDM ou Sessão inválida
+                return resp;
+            }
+            
+            if(!json.UDSObjectList.UDSObject){
+                // Sessão não encontrada
+                return resp;
+            }
+
+            resp.status = 1;
+        }catch(e){
+            // TODO: Verify Session error (1010)
+            const errorCode = Util.xPath('//ErrorCode', e.response.data)[0];
+            resp.status = 1;
+        }
+
+        return resp;
     }
 }
 
