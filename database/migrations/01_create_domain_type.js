f74db6d776283361661afa8dc2f2ba1c42677167
Rafael Coelho <rafaelcoelho8@yahoo.com.br>
2020-08-04 00:29:46 -0300

    updated: migrations

--- database/migrations/01_create_domain_type.js
+++ database/migrations/01_create_domain_type.js
@@ -4,7 +4,7 @@ exports.up = async function(knex, utf8 = false) {
             table.collate('utf8_unicode_ci');
         table.string('id', 45).primary();
         table.string('name', 50).notNullable();
-        table.string('description', 200).notNullable();
+        table.string('description', 200);
         table.boolean('deleted');
     });
 }
