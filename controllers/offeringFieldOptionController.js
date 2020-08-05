c3b77b5dbaa7c5665a72167bd3b2dc4650855a09
Rafael Coelho <rafaelcoelho8@yahoo.com.br>
2020-08-05 19:08:44 -0300

    added: Update Offerin Field Option

--- controllers/offeringFieldOptionController.js
+++ controllers/offeringFieldOptionController.js
@@ -72,4 +72,67 @@ module.exports = (app) => {
         res.send(resp);
     });
 
+    // [PUT] => /OfferingFieldOption/:id
+    app.put('/OfferingFieldOption/:id', async (req, res) => {
+        const { headers, body, params } = req;
+        const resp = {
+            status: 0,
+            msg: "",
+            data: null,
+            errors: []
+        };
+
+        if (!headers['authorization'] || !Number(headers['authorization'])) {
+            resp.errors.push({
+                location: "header",
+                param: "Authorization",
+                msg: "A Session ID precisa ser informada!"
+            });
+            return res.status(403).send(resp);
+        }
+        
+        // TODO: Session verification 
+        const sid = headers['authorization'];
+
+        const field = await OfferingFieldOption.GetFirst(`id = '${params.id}' AND deleted = 0`);
+
+        if(!field){
+            resp.errors.push({
+                msg: "Opção não encontrada"
+            });
+            return res.status(404).send(resp);
+        }
+
+        const payload = {};
+        let edit = false;
+        OfferingFieldOption.fields.forEach(_field => {
+            if(body[_field] && ![ 'id', 'field' ].includes(_field)){
+                payload[_field] = body[_field];
+                edit = true;
+            }
+        });
+
+        if(!edit){
+            resp.errors.push({
+                msg: "Nada para ser editado"
+            });
+            return res.send(resp);
+        }
+
+        const update = await OfferingFieldOption.Update(payload, `id = '${params.id}'`);
+
+        if(update.status !== 1){
+            resp.errors.push({
+                msg: "Erro ao atualizar opção!"
+            });
+            return res.status(500).send(resp);
+        }
+
+        resp.status = 1;
+        resp.msg = "Opção atualizada com sucesso!";
+        resp.data = { ...field, ...payload };
+        res.send(resp);
+
+    });
+
 };
