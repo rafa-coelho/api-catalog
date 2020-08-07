2c13f1167e9ed479616f0ccd7b401c1b63b86146
Rafael Coelho <rafaelcoelho8@yahoo.com.br>
2020-08-06 23:01:43 -0300

    added: Request.Create overload

--- classes/Request.js
+++ classes/Request.js
@@ -3,6 +3,13 @@ class Request extends Classes
     static entity = "req";
     static table = "request";
     static fields = [ "id", "user", "offering", "summary", "status", "created_at", "updated_at" ];
+
+    static Create(data){
+        data._status = "created";
+        data.created_at = new Date().toJSON();
+        return Classes.Create.bind(this)(data);
+    }
+
 }
 
 module.exports = Request;
