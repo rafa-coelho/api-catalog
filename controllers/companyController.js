0073b9cea05bcb63e4a89f4b95fbe9504f8e1b4a
Rafael Coelho <rafaelcoelho8@yahoo.com.br>
2020-09-24 20:21:56 -0300

    added: get company by ID

--- controllers/companyController.js
+++ controllers/companyController.js
@@ -65,11 +65,11 @@ module.exports = (app) => {
             return res.status(400).send(resp);
         }
 
-        const grupoExiste = await Company.GetFirst(`name = '${body.name}'`);
+        const companyExiste = await Company.GetFirst(`name = '${body.name}'`);
 
-        if(grupoExiste){
+        if(companyExiste){
             resp.errors.push({
-                msg: "Já existe um grupo com este nome"
+                msg: "Já existe uma empresa com este nome"
             });
             return res.status(401).send(resp);
         }
@@ -83,14 +83,50 @@ module.exports = (app) => {
 
         if(create.status !== 1){
             resp.errors.push({
-                msg: "Erro ao criar grupo"
+                msg: "Erro ao criar Empresa"
             });
             return res.status(500).send(resp);
         }
 
         resp.status = 1;
         resp.data = data;
-        resp.msg = "Grupo criado com sucesso!";
+        resp.msg = "Empresa criada com sucesso!";
         res.send(resp);
     });
+
+    // [GET] => company/:id
+    app.get(`/company/:id`, async (req, res) => {
+        const { headers, params } = req;
+        const resp = {
+            status: 0,
+            msg: "",
+            data: null,
+            errors: []
+        };
+
+        const session = await Session.Validar(headers['authorization'], 'get.company');
+
+        if (!session.status) {
+            resp.errors.push({
+                location: "header",
+                param: "Authorization",
+                msg: session.msg
+            });
+            return res.status(403).send(resp);
+        }
+
+        const company = await Company.GetFirst(`id = '${params.id}'`);
+
+        if (!company) {
+            resp.errors.push({
+                msg: "Empresa não encontrada"
+            });
+            return res.status(404).send(resp);
+        }
+
+        resp.status = 1;
+        resp.data = company;
+        res.send(resp);
+    });
+
 };
