55f0b64d45dce8a85c35b98cc8d5bd6853727a68
Rafael Coelho <rafaelcoelho8@yahoo.com.br>
2020-09-24 17:46:13 -0300

    added: user seed

---
+++ database/seeds/02_user.js
@@ -0,0 +1,14 @@
+module.exports = {
+    table: "user",
+    values: [
+        {
+            id: '4c30d8b0-fea5-11ea-9e9d-6fe810a04eb9',
+            name: 'Admin',
+            email: 'admin@luminetec.com.br',
+            username: 'admin@luminetec.com.br',
+            password: 'U2FsdGVkX1+AE0gGqzwaoSYiuhMcUmc4825JjGGsG90=',
+            access_type: 'Administrator',
+            status: 1
+        }
+    ]
+}
