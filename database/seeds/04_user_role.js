101b05501e33c6d167c29105dae8c77343ad38ef
Rafael Coelho <rafaelcoelho8@yahoo.com.br>
2020-09-24 17:54:52 -0300

    added: user role seed

---
+++ database/seeds/04_user_role.js
@@ -0,0 +1,15 @@
+module.exports = {
+    table: "user_role",
+    values: [
+        {
+            id: 'b730e590-fea7-11ea-8947-7b3e3fb5053f',
+            user: '4c30d8b0-fea5-11ea-9e9d-6fe810a04eb9',
+            role: '249cef4e-ae7c-4185-b45f-5816d9f827df'
+        },
+        {
+            id: 'b730be80-fea7-11ea-8947-7b3e3fb5053f',
+            user: '4c30d8b0-fea5-11ea-9e9d-6fe810a04eb9',
+            role: '031df649-fe3f-461f-80f0-52b6da4d405f'
+        }
+    ]
+};
