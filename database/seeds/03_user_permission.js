e32466ac1792b605fc5c5c15fb2b07a0d32217d1
Rafael Coelho <rafaelcoelho8@yahoo.com.br>
2020-09-24 17:46:23 -0300

    added: user permission seed

---
+++ database/seeds/03_user_permission.js
@@ -0,0 +1,160 @@
+module.exports = {
+    table: "user_permission",
+    values: [
+        {
+            id: '4c8401c8-fea5-11ea-9e9d-6fe810a04eb9',
+            user: '4c30d8b0-fea5-11ea-9e9d-6fe810a04eb9',
+            action: 'post.logout'
+        },
+        {
+            id: '4c8401c7-fea5-11ea-9e9d-6fe810a04eb9',
+            user: '4c30d8b0-fea5-11ea-9e9d-6fe810a04eb9',
+            action: 'get.DomainType'
+        },
+        {
+            id: '4c8401c6-fea5-11ea-9e9d-6fe810a04eb9',
+            user: '4c30d8b0-fea5-11ea-9e9d-6fe810a04eb9',
+            action: 'put.request'
+        },
+        {
+            id: '4c8401c5-fea5-11ea-9e9d-6fe810a04eb9',
+            user: '4c30d8b0-fea5-11ea-9e9d-6fe810a04eb9',
+            action: 'get.request'
+        },
+        {
+            id: '4c8401c4-fea5-11ea-9e9d-6fe810a04eb9',
+            user: '4c30d8b0-fea5-11ea-9e9d-6fe810a04eb9',
+            action: 'delete.request'
+        },
+        {
+            id: '4c8401c3-fea5-11ea-9e9d-6fe810a04eb9',
+            user: '4c30d8b0-fea5-11ea-9e9d-6fe810a04eb9',
+            action: 'get.offering'
+        },
+        {
+            id: '4c8401c2-fea5-11ea-9e9d-6fe810a04eb9',
+            user: '4c30d8b0-fea5-11ea-9e9d-6fe810a04eb9',
+            action: 'post.login'
+        },
+        {
+            id: '4c8401c1-fea5-11ea-9e9d-6fe810a04eb9',
+            user: '4c30d8b0-fea5-11ea-9e9d-6fe810a04eb9',
+            action: 'post.request'
+        },
+        {
+            id: '4c8401c0-fea5-11ea-9e9d-6fe810a04eb9',
+            user: '4c30d8b0-fea5-11ea-9e9d-6fe810a04eb9',
+            action: 'get.DomainValue'
+        },
+        {
+            id: '4c5c5585-fea5-11ea-9e9d-6fe810a04eb9',
+            user: '4c30d8b0-fea5-11ea-9e9d-6fe810a04eb9',
+            action: 'put.DomainValue'
+        },
+        {
+            id: '4c5c5584-fea5-11ea-9e9d-6fe810a04eb9',
+            user: '4c30d8b0-fea5-11ea-9e9d-6fe810a04eb9',
+            action: 'post.login'
+        },
+        {
+            id: '4c5c5583-fea5-11ea-9e9d-6fe810a04eb9',
+            user: '4c30d8b0-fea5-11ea-9e9d-6fe810a04eb9',
+            action: 'post.DomainType'
+        },
+        {
+            id: '4c5c5582-fea5-11ea-9e9d-6fe810a04eb9',
+            user: '4c30d8b0-fea5-11ea-9e9d-6fe810a04eb9',
+            action: 'post.OfferingFieldOption'
+        },
+        {
+            id: '4c5c5581-fea5-11ea-9e9d-6fe810a04eb9',
+            user: '4c30d8b0-fea5-11ea-9e9d-6fe810a04eb9',
+            action: 'get.DomainValue'
+        },
+        {
+            id: '4c5c5580-fea5-11ea-9e9d-6fe810a04eb9',
+            user: '4c30d8b0-fea5-11ea-9e9d-6fe810a04eb9',
+            action: 'post.DomainValue'
+        },
+        {
+            id: '4c5c2e7f-fea5-11ea-9e9d-6fe810a04eb9',
+            user: '4c30d8b0-fea5-11ea-9e9d-6fe810a04eb9',
+            action: 'put.DomainType'
+        },
+        {
+            id: '4c5c2e7e-fea5-11ea-9e9d-6fe810a04eb9',
+            user: '4c30d8b0-fea5-11ea-9e9d-6fe810a04eb9',
+            action: 'get.offering'
+        },
+        {
+            id: '4c5c2e7d-fea5-11ea-9e9d-6fe810a04eb9',
+            user: '4c30d8b0-fea5-11ea-9e9d-6fe810a04eb9',
+            action: 'get.DomainType'
+        },
+        {
+            id: '4c5c2e7c-fea5-11ea-9e9d-6fe810a04eb9',
+            user: '4c30d8b0-fea5-11ea-9e9d-6fe810a04eb9',
+            action: 'put.request'
+        },
+        {
+            id: '4c5c2e7b-fea5-11ea-9e9d-6fe810a04eb9',
+            user: '4c30d8b0-fea5-11ea-9e9d-6fe810a04eb9',
+            action: 'delete.OfferingFieldOption'
+        },
+        {
+            id: '4c5c2e7a-fea5-11ea-9e9d-6fe810a04eb9',
+            user: '4c30d8b0-fea5-11ea-9e9d-6fe810a04eb9',
+            action: 'put.OfferingField'
+        },
+        {
+            id: '4c5c2e79-fea5-11ea-9e9d-6fe810a04eb9',
+            user: '4c30d8b0-fea5-11ea-9e9d-6fe810a04eb9',
+            action: 'get.request'
+        },
+        {
+            id: '4c5c2e78-fea5-11ea-9e9d-6fe810a04eb9',
+            user: '4c30d8b0-fea5-11ea-9e9d-6fe810a04eb9',
+            action: 'post.OfferingField'
+        },
+        {
+            id: '4c5c2e77-fea5-11ea-9e9d-6fe810a04eb9',
+            user: '4c30d8b0-fea5-11ea-9e9d-6fe810a04eb9',
+            action: 'delete.request'
+        },
+        {
+            id: '4c5c2e76-fea5-11ea-9e9d-6fe810a04eb9',
+            user: '4c30d8b0-fea5-11ea-9e9d-6fe810a04eb9',
+            action: 'delete.OfferingField'
+        },
+        {
+            id: '4c5c2e75-fea5-11ea-9e9d-6fe810a04eb9',
+            user: '4c30d8b0-fea5-11ea-9e9d-6fe810a04eb9',
+            action: 'delete.offering'
+        },
+        {
+            id: '4c5c2e74-fea5-11ea-9e9d-6fe810a04eb9',
+            user: '4c30d8b0-fea5-11ea-9e9d-6fe810a04eb9',
+            action: 'post.offering'
+        },
+        {
+            id: '4c5c2e73-fea5-11ea-9e9d-6fe810a04eb9',
+            user: '4c30d8b0-fea5-11ea-9e9d-6fe810a04eb9',
+            action: 'post.logout'
+        },
+        {
+            id: '4c5c2e72-fea5-11ea-9e9d-6fe810a04eb9',
+            user: '4c30d8b0-fea5-11ea-9e9d-6fe810a04eb9',
+            action: 'put.offering'
+        },
+        {
+            id: '4c5c2e71-fea5-11ea-9e9d-6fe810a04eb9',
+            user: '4c30d8b0-fea5-11ea-9e9d-6fe810a04eb9',
+            action: 'put.OfferingFieldOption'
+        },
+        {
+            id: '4c5c2e70-fea5-11ea-9e9d-6fe810a04eb9',
+            user: '4c30d8b0-fea5-11ea-9e9d-6fe810a04eb9',
+            action: 'post.request'
+        }
+    ]
+};
