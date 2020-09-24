c0916bef44abc38a2676af93ae4c9b04a525d01f
Rafael Coelho <rafaelcoelho8@yahoo.com.br>
2020-09-24 20:11:13 -0300

    updated: group to company Controller

--- controllers/companyController.js
+++ controllers/companyController.js
@@ -1,7 +1,7 @@
 module.exports = (app) => {
 
-    // [GET] => /group
-    app.get(`/group`, async (req, res) => {
+    // [GET] => /company
+    app.get(`/company`, async (req, res) => {
         const { headers, query } = req;
         const resp = {
             status: 0,
@@ -10,7 +10,7 @@ module.exports = (app) => {
             errors: []
         };
 
-        const session = await Session.Validar(headers['authorization'], 'get.group');
+        const session = await Session.Validar(headers['authorization'], 'get.company');
 
         if (!session.status) {
             resp.errors.push({
@@ -21,24 +21,19 @@ module.exports = (app) => {
             return res.status(403).send(resp);
         }
 
-        
-        const userRoles = await Role.GetUserRoles(session.data.user);
-        
-
-
         const where = (query.where) ? query.where : "";
         const order_by = (query.order_by) ? query.order_by : "";
         const limit = (query.limit) ? query.limit : "";
 
-        const requests = await Group.Get(where, order_by, limit);
+        const requests = await Company.Get(where, order_by, limit);
 
         resp.status = 1;
         resp.data = requests;
         res.send(resp);
     });
 
-    // [POST] => /group
-    app.post(`/group`, async (req, res) => {
+    // [POST] => /company
+    app.post(`/company`, async (req, res) => {
         const { headers, body } = req;
         const resp = {
             status: 0,
@@ -47,7 +42,7 @@ module.exports = (app) => {
             errors: []
         };
 
-        const session = await Session.Validar(headers['authorization'], 'post.group');
+        const session = await Session.Validar(headers['authorization'], 'post.company');
 
         if (!session.status) {
             resp.errors.push({
@@ -70,7 +65,7 @@ module.exports = (app) => {
             return res.status(400).send(resp);
         }
 
-        const grupoExiste = await Group.GetFirst(`name = '${body.name}'`);
+        const grupoExiste = await Company.GetFirst(`name = '${body.name}'`);
 
         if(grupoExiste){
             resp.errors.push({
@@ -84,7 +79,7 @@ module.exports = (app) => {
             name: body.name
         };
 
-        const create = await Group.Create(data);
+        const create = await Company.Create(data);
 
         if(create.status !== 1){
             resp.errors.push({
