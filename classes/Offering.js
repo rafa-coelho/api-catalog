21cfd649fa071a4a21612f1c5cd140504831ec6f
Rafael Coelho <rafaelcoelho8@yahoo.com.br>
2020-10-10 01:09:24 -0300

    updated: offering

--- classes/Offering.js
+++ classes/Offering.js
@@ -1,7 +1,7 @@
 class Offering extends Classes{
 
     static table = 'offering';
-    static fields = [ 'id', 'name', 'company', 'external_id' ];
+    static fields = [ 'id', 'name', 'company', 'type', 'external_id' ];
 
 }
 
